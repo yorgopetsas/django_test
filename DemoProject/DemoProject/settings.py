3e1952d41334eb1e7c4d2c1fd4673eb0a2c71812
Yorgo Petsas <105683509+yorgopetsas@users.noreply.github.com>
2024-03-19 07:20:00 +0000

    allauth

--- DemoProject/DemoProject/settings.py
+++ DemoProject/DemoProject/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+ 
+)
+SITE_ID = 1

1874b2b5420bc62c9ad4f7fd26e6f3dd2f5d7691
Yorgo Petsas <105683509+yorgopetsas@users.noreply.github.com>
2024-03-22 15:14:37 +0000

    Back after 12 steps, cart started

--- back/ecom/ecom/settings.py
+++ back/ecom/ecom/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "store", 
+    "store",
+    "cart",
 ]
 
 MIDDLEWARE = [
---
+++ back/ecom/store/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render, redirect
+from .models import Product, Category
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages 
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, "home.html", {"products":products})
+
+def about(request):
+    return render(request, "about.html", {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Login errror, please try again"))
+            return redirect('login')
+    else:
+        return render(request, "login.html", {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out"))
+    return redirect("home")
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # log in user Have to disable
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Your registrarion application was submitted."))
+            return redirect("home")
+        else:
+            messages.success(request, ("Something went wrong, please try again."))
+            return redirect("register")
+    else:
+        return render(request, "register.html", {'form':form})
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, "product.html", {"product":product})
+
+def category(request, foo):
+    foo = foo.replace('-', ' ')
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products':products, 'category':category})
+    except:
+        messages.success(request("That Category does not exist"))
+        return redirect('home')

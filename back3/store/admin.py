9fb492e1be74bf33db19eea51c037692ff19db04
Yorgo Petsas <105683509+yorgopetsas@users.noreply.github.com>
2024-03-26 22:34:06 +0000

    Stable Version after User Profile is created

---
+++ back3/store/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Category, Customer, Product, Order, Profile
+from django.contrib.auth.models import User
+
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Profile)
+
+# Mix profile info and user info
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+# Extend User Model
+    
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ["username", "first_name", "last_name", "email",]
+    inlines = [ProfileInline]
+
+# Unregister old way user
+admin.site.unregister(User)
+
+# Re-register with new user
+admin.site.register(User, UserAdmin)
---
+++ back3/store/views.py
@@ -0,0 +1,114 @@
+from django.shortcuts import render, redirect
+from .models import Product, Category
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages 
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
+from django import forms
+
+def update_password(request):
+    if request.user.is_authenticated:
+        current_user = request.user
+        if request.method == "POST":
+            form = ChangePasswordForm(current_user, request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Your Password has been updated")
+                login(request, current_user)
+                return redirect("update_user")
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request, error)
+                    return redirect("update_password")
+        else:
+            form = ChangePasswordForm(current_user)
+            return render(request, "update_password.html", {"form": form})
+    else:
+        messages.success(request, "You must be logged in to see that pagePassword has been updated")
+        return redirect("home")
+
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+        if user_form.is_valid():
+            user_form.save()
+
+            login(request, current_user)
+            messages.success(request, "User has been updated!")
+            return redirect('home')
+        
+        return render(request, "update_user.html", {'user_form': user_form})
+    else:
+        messages.success(request, "You must be logged in to access this page")
+        return redirect('home')
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, "home.html", {"products":products})
+
+def about(request):
+    return render(request, "about.html", {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Login errror, please try again"))
+            return redirect('login')
+    else:
+        return render(request, "login.html", {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out"))
+    return redirect("home")
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # log in user Have to disable
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Your registrarion application was submitted."))
+            return redirect("home")
+        else:
+            messages.success(request, ("Something went wrong, please try again."))
+            return redirect("register")
+    else:
+        return render(request, "register.html", {'form':form})
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, "product.html", {"product":product})
+
+def category(request, foo):
+    foo = foo.replace('-', ' ')
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products':products, 'category':category})
+    except:
+        messages.success(request("That Category does not exist"))
+        return redirect('home')
+    
+def category_summary(request):
+    categories = Category.objects.all()
+    return render(request, "category_summary.html", {"categories": categories})
+ 
+     
+

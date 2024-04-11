9ac297835fda6742fe31bcd9b81e69e9412064d5
yorgopecas@gmail.com <105683509+yorgopetsas@users.noreply.github.com>
2024-04-11 08:11:34 +0000

    Start Checkout Page

--- ecom/payment/forms.py
+++ ecom/payment/forms.py
@@ -16,3 +16,15 @@ class ShippingForm(forms.ModelForm):
         fields = ["shipping_full_name", "shipping_email", "shipping_address1", "shipping_address2", "shipping_city", "shipping_state", "shipping_zipcode", "shipping_country"]
 
         exclude = ['user',]
+
+class PaymentForm(forms.Form):
+    card_name =forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-contro', 'placeholder': 'Name on Card'}), required=True)
+    card_number =forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-contro', 'placeholder': 'Card Number'}), required=True)
+    card_exp_date =forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-contro', 'placeholder': 'Expiration Date'}), required=True)
+    card_cvv_number =forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-contro', 'placeholder': 'CVV Number'}), required=True)
+    card_address1 =forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-contro', 'placeholder': 'Card Address 1'}), required=True)
+    card_address2 =forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-contro', 'placeholder': 'Card Address 2'}), required=False)
+    card_city =forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-contro', 'placeholder': 'Card City'}), required=True)
+    card_state =forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-contro', 'placeholder': 'Card State'}), required=True)
+    card_zipcode =forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-contro', 'placeholder': 'Card Postal Code'}), required=True)
+    card_country =forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-contro', 'placeholder': 'Card Country'}), required=True)
--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -1,18 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cart.cart import Cart
-from .forms import ShippingForm
+from .forms import ShippingForm, PaymentForm
 from .models import ShippingAddress
+from django.contrib import messages 
+
+
+def billing_info(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.totals()
+
+        # Check if logged
+        if request.user.is_authenticated:
+            billing_form = PaymentForm()
+            return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_info":request.POST, "billing_form": billing_form})
+
+        else:
+            billing_form = PaymentForm()
+            return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_info":request.POST, "billing_form": billing_form})
+
+        # shipping_form = request.POST
+        # return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_form":shipping_form})
+    else:
+        messages.success(request, "Access Denied. Please login")
+        return redirect('home')
 
 def checkout(request):
-     # Get the cart
     cart = Cart(request)
     cart_products = cart.get_prods
     quantities = cart.get_quants
     totals = cart.totals()
 
-    shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
-
     if request.user.is_authenticated:
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
         shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
         return render(request, "payment/checkout.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_form": shipping_form})
     else:

72477809729fe748e4155e5f633ce4560cb35416
yorgopecas@gmail.com <105683509+yorgopetsas@users.noreply.github.com>
2024-05-17 13:48:06 +0000

    Shipped Orders Dashboard finished

--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -9,14 +9,17 @@ from store.models import Product
 
 def not_shipped_dash(request):
     if request.user.is_authenticated and request.user.is_superuser:
-        return render(request, "payment/not_shipped_dash.html", {})
+        orders = Order.objects.filter(shipped=False)
+
+        return render(request, "payment/not_shipped_dash.html", {"orders":orders})
     else:
         messages.success(request, "Access Denied")
         return redirect('home')
 
 def shipped_dash(request):
     if request.user.is_authenticated and request.user.is_superuser:
-        return render(request, "payment/shipped_dash.html", {})
+        orders = Order.objects.filter(shipped=True)
+        return render(request, "payment/shipped_dash.html", {"orders":orders})
     else:
         messages.success(request, "Access Denied")
         return redirect('home')

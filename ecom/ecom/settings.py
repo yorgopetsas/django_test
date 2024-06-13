c73d439de6e2d9fe79b209220e89c34aeee3a3d8
yorgopecas@gmail.com <105683509+yorgopetsas@users.noreply.github.com>
2024-06-13 14:52:43 +0000

    Install python-dotenv. Created env variable. Include it in gitignore.

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": 'railway',
         "USER": 'postgres',
-        "PASSWORD": '',
+        "PASSWORD": os.environ.get('DB_PASSWORD_YO'),
         "HOST": 'roundhouse.proxy.rlwy.net',
         "PORT": '39532',
     }
